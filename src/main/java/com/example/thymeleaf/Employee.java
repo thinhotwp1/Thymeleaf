61b9592bf2b2b2e91052f480356e0b0d4e1d3699
ThinhLD3 <leduythinh20001@gmail.com>
2023-07-26 13:50:41 +0700

    add

---
+++ src/main/java/com/example/thymeleaf/Employee.java
@@ -0,0 +1,15 @@
+package com.example.thymeleaf;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    private long id;
+    private String name;
+    private int age;
+    private String description;
+}
